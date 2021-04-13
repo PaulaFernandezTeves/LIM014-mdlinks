944fda186345f3e0ce72257b66d66fd2b6e93309
PaulaFernandezTeves <paulafernandezteves@gmail>
2021-04-13 11:22:24 -0500

    test y path

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -37,7 +37,7 @@ const {
   relativeToAbsolute,
   fileExist,
   //isMdFile,
-  //searchLinks,
+  searchLinks,
   //validateLinks,
 
 } = require ("../src/index.js");
@@ -77,4 +77,35 @@ describe('Función si es archivo o directorio', () => {
   });
 });
 
+describe("Función para trabjar con HTML y extraer links ", () => {
+  test("debería retornar los links de los enlace ", () => {
+    expect(searchLinks("C:\\Users\\user\\LIM014-mdlinks\\pruebas\\prueba1\\prueba3.md")).toEqual([
+      {
+        file: "C:\\Users\\user\\LIM014-mdlinks\\pruebas\\prueba1\\prueba3.md",
+        href: 'https://www.geeksforgeeks.org/nodejs-web-crawling-using-cheerio/',
+        text: '"GeeksforGeeks, using cheerio  - Articulo"',
+      },
+    ]);
+  });
+});
 
+
+/*describe('validar links de array', () => {
+  it('is a function', () => {
+    expect(typeof md.validateLinks).toBe('function');
+  });
+  it('return status "Internal Server Error"', (done) => {
+    md.validateLinks(arraylinkInValid)
+      .then((element) => {
+        expect(element[0].message).toBe('Internal Server Error');
+        done();
+      });
+  });
+  it('return status 200', (done) => {
+    md.validateLinks(arraylinkValid)
+      .then((element) => {
+        expect(element[0].status).toEqual(200);
+        done();
+      });
+  });
+});*/
